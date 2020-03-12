0f2966b82f325201d0c613584664e61f6732ce92
Viktor Nikolaev <viktor.nikolaev@thomsonreuters.com>
2020-03-12 19:15:58 +0300

    main function

--- Otus.Interfaces/Program.cs
+++ Otus.Interfaces/Program.cs
@@ -4,6 +4,34 @@
   {
     private static void Main(string[] args)
     {
+      ISerializer serializer = new OtusXmlSerializer();
+      
+      var game = new Game(serializer);
+
+      IGameObject human = new Human();
+      game.AddObject(human);
+      
+      game.Save();
+      
+      //========================================================
+      IApiClientFactory apiClientFactory = new ApiClientFactory();
+
+      IApiClient client1 = apiClientFactory.CreateClient("http://our.api.com");
+      IApiClient client2 = apiClientFactory.CreateClient("ws://our.api.com");
+
+      // добавляем логгирование
+      client1 = new ApiClientWithLogging(client1);
+      client2 = new ApiClientWithLogging(client2);
+      
+      // вызываем методы
+      client1.SendRequest();
+      client2.SendRequest();
+
+      //=============
+
+
+      IReadRepository<object> r = new ReadRepository<string>();
+      IWriteRepository<string> w = new WriteRepository<object>();
     }
   }
 }
